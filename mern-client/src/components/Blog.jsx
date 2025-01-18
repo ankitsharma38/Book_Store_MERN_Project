1c6fe6129f7baf38820ac1846ba274b6595802bd
AnkitSharma <Ankitsharma7805@gmail.com>
2025-01-18 12:09:11 +0530

    blogs

--- mern-client/src/components/Blog.jsx
+++ mern-client/src/components/Blog.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 const Blog = () => {
   return (
-    <div>Blog</div>
-  )
-}
-export default Blog
+    <div style={{ 
+      display: 'flex',justifyContent: 'center',alignItems: 'center',height: '100vh',
+      flexDirection: 'column',textAlign: 'center' 
+    }}>
+      <h1 style={{ fontSize: '2rem', color: '#333' }}>Page Under Maintenance</h1>
+      <p style={{ fontSize: '1.2rem', color: '#555' }}>
+        We are currently working on this page to bring you an improved experience. <br />
+        Please check back later!
+      </p>
+    </div>
+  );
+};
+
+export default Blog;
