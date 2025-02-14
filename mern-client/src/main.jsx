0e8443ff14b952a4f3ccc3ac8f910d4f99ae75ac
AnkitSharma <ankitsharma7805@gmail.com>
2025-02-14 16:52:49 +0530

    main

--- mern-client/src/main.jsx
+++ mern-client/src/main.jsx
@@ -6,10 +6,13 @@ import { RouterProvider } from 'react-router-dom'
 import router from './routers/router.jsx'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import AuthProvider from './context/AuthProvider.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-        <RouterProvider router={router} />
+        <AuthProvider>
+          <RouterProvider router={router} />
+        </AuthProvider>
   </React.StrictMode>,
 )
